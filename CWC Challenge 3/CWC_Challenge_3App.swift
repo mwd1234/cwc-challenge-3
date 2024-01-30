3f3849dbd3e528f77524794fcfe1e8dfc13a5b47
Matthew Dembinski <matthewdembinski@gmail.com>
2024-01-30 13:59:53 -0700

    Initial Commit

---
+++ CWC Challenge 3/CWC_Challenge_3App.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_Challenge_3App.swift
+//  CWC Challenge 3
+//
+//  Created by Matthew Dembinski on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CWC_Challenge_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
